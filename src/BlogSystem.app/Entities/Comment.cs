01f23f188b4b2585156bfcb7749fa05775651411
mtpachecoo <matheusalmeida13@hotmail.com>
2026-02-03 17:53:09 -0300

    feat: implement logic to display post and comments.

---
+++ src/BlogSystem.app/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogSystem.app.Entities
+{
+    public class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(){}
+        public Comment(string text)
+        {
+            Text = text;
+        }   
+
+    }
+}
